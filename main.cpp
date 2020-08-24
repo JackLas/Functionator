229c66f36b380cece0a65c4b90eac8f6a09b1f9a
JackLas <jackgm555@gmail.com>
2020-08-24 23:42:15 +0300

    POC: initial commit

---
+++ main.cpp
@@ -0,0 +1,85 @@
+#include <iostream> 
+#include <string>
+
+#define VARNAME(VAR) (#VAR)
+
+const char c_space = ' ';
+const char c_openPrioBrac = '(';
+const char c_closePrioBrac = ')';
+
+const std::string test_start = " 1 + 2 + 3  2 * 6 / 2 -       3 -     4";
+const std::string test_spaces = "1+2+32*6/2-3-4";
+const std::string test_prio = "(1+(2+((3*6)/2))-3)-4";
+
+std::string getStringFromUser()
+{
+	std::cout << "Please enter your function:" << std::endl;
+	std::string result;
+	std::getline(std::cin, result);
+	return result;
+}
+
+void printFunc(const std::string& str)
+{
+	std::cout << "'"
+			  << str
+			  << "'"
+			  << std::endl;
+}
+
+void test(const std::string& input, const std::string& check, 
+		  const std::string testName = "", bool verbose = false)
+{
+	std::cout << std::endl;
+	std::cout << (testName.size() > 0 ? testName + ": " : "");
+	if(input == check)
+	{
+		std::cout << "Passed" << std::endl;
+	}
+	else
+	{
+		std::cout << "Error" << std::endl;
+	}
+
+	if(verbose)
+	{
+		std::cout << "\t"
+				  << "'" + input + "'"
+				  << " == "
+				  << "'" + check + "'"
+				  << std::endl;
+	}
+}
+
+void removeSpaces(std::string& str)
+{
+	for(auto c = str.end(); c >= str.begin();--c)
+	{
+		if (*c == c_space)
+		{
+			str.erase(c);
+		}
+	}
+}
+
+void setPriorityBrackets(std::string& str)
+{ 
+	for(auto c = str.begin(); c < str.end(); c++)
+	{
+		// ! TODO !
+	}
+}
+
+int main(int argc, char** argv)
+{
+	//std::string funcStr = getStringFromUser();
+	std::string funcStr = test_start;
+
+	removeSpaces(funcStr);
+	test(funcStr, test_spaces, VARNAME(test_spaces), true);
+
+	setPriorityBrackets(funcStr);
+	test(funcStr, test_prio, VARNAME(test_prio), true);
+
+	return 0;
+}
